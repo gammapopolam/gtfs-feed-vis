9bbe1a1f5b8110b46b05cb1aa45a613f3032a3e1
Ivan Gamma <56549605+gammapopolam@users.noreply.github.com>
2024-07-13 22:17:26 +0300

    add loom and mvt decoder support

--- feed.py
+++ feed.py
@@ -1,17 +1,250 @@
 import pandas as pd
+import math
+import geopandas as gpd
+import warnings
+from pandas.errors import SettingWithCopyWarning
+import subprocess
+import os
+import mapbox_vector_tile
+
+warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
+class Loom:
+    # Init with Feed to get selection by stop
+    def __init__(self, folder: str, stop_id):
+        self.stop_id=stop_id
+        self.folder=folder
+        feed=Feed(folder)
+        selection_folder=feed.SaveSelectionOfStop(stop_id, folder)
+        gtfs_folder=selection_folder+'/gtfs_edited'
+        self.agency=pd.read_csv(gtfs_folder+'/agency.txt', sep=',')
+        self.calendar=pd.read_csv(gtfs_folder+'/calendar.txt', sep=',')
+        self.calendar_dates=pd.read_csv(gtfs_folder+'/calendar_dates.txt', sep=',')
+        self.routes=pd.read_csv(gtfs_folder+'/routes.txt', sep=',')
+        self.trips=pd.read_csv(gtfs_folder+'/trips.txt', sep=',')
+        stops=pd.read_csv(gtfs_folder+'/stops.txt', sep=',')
+        self.stops=gpd.GeoDataFrame(stops, geometry=gpd.points_from_xy(stops.stop_lon, stops.stop_lat), crs="EPSG:4326")
+        #self.stops=pd.read_csv(folder+'/stops.txt', sep=',')
+        self.stop_times=pd.read_csv(gtfs_folder+'/stop_times.txt', sep=',')
+        shapes=pd.read_csv(gtfs_folder+'/shapes.txt', sep=',')
+        self.shapes=gpd.GeoDataFrame(shapes, geometry=gpd.points_from_xy(shapes.shape_pt_lon, shapes.shape_pt_lat))
+        self.selection_folder=selection_folder
+    def gtfs2graph(self, mots=['all']):
+        try:
+            gtf2graph_subp=subprocess.check_output(f'gtfs2graph {self.selection_folder}/gtfs_edited --mots {",".join(mots)} > {self.selection_folder}/gtfs2graph.json', shell=True)                       
+        except subprocess.CalledProcessError as exc:                                                                                                   
+            print("error code", exc.returncode, exc.output)
+    def topo(self, max_aggr_dist=50, no_infer_restr=False, infer_restr_max_dist=50, max_comp_dist=10000, smooth=0):
+        if no_infer_restr==False:
+            try:
+                #print(f'topo --max-aggr-dist={max_aggr_dist} --max-comp-dist={max_comp_dist} --smooth={smooth} < {self.selection_folder}/gtfs2graph.json > {self.selection_folder}/topo.json')
+                #print(subprocess.run(f'topo --max-aggr-dist={max_aggr_dist} --max-comp-dist={max_comp_dist} --smooth={smooth} < {self.selection_folder}/gtfs2graph.json > {self.selection_folder}/topo.json', shell=True))
+                topo_subp=subprocess.check_output(f'topo --max-aggr-dist={max_aggr_dist} --max-comp-dist={max_comp_dist} --smooth={smooth} < {self.selection_folder}/gtfs2graph.json > {self.selection_folder}/topo.json', shell=True)                       
+            except subprocess.CalledProcessError as exc:                                                                                                   
+                print("error code", exc.returncode, exc.output)
+        else:
+            try:
+                topo_subp=subprocess.check_output(f'topo --max-aggr-dist={max_aggr_dist} --no-infer-restr --infer-restr-max-dist={infer_restr_max_dist} --max-comp-dist={max_comp_dist} --smooth={smooth} < {self.selection_folder}/gtfs2graph.json > {self.selection_folder}/topo.json', shell=True)                       
+            except subprocess.CalledProcessError as exc:                                                                                                   
+                print("error code", exc.returncode, exc.output)
+    def loom(self, optim_method='hillc', no_prune=False, no_untangle=False, same_seg_cross_pen=4, diff_seg_cross_pen=1, in_stat_cross_pen_same_seg=12, in_stat_cross_pen_diff_seg=3, sep_pen=3, in_stat_sep_pen=9, ilp=False, ilp_solver='cbc', ilp_num_threads=4):
+        loom_cfg=f'loom --optim-method'
+        if ilp==True:
+            loom_cfg+=f' ilp --ilp-solver={ilp_solver} --ilp-num-threads={ilp_num_threads}'
+        if no_prune==True:
+            loom_cfg+=f' --no-prune'
+        if no_untangle==True:
+            loom_cfg+=f' --no-untangle'
+        loom_cfg+=f' --same-seg-cross-pen={same_seg_cross_pen} --diff-seg-cross-pen={diff_seg_cross_pen} --in-stat-cross-pen-same-seg={in_stat_cross_pen_same_seg} --in-stat-cross-pen-diff-seg={in_stat_cross_pen_diff_seg} --in-stat-sep-pen={in_stat_sep_pen} --sep-pen={sep_pen}'
+        loom_cfg+=f' < {self.selection_folder}/topo.json> {self.selection_folder}/loom.json'
+        try:
+            loom_subp=subprocess.check_output(loom_cfg, shell=True)
+        except subprocess.CalledProcessError as exc:                                                                                                   
+            print("error code", exc.returncode, exc.output)
+    def transitmap(self, render_engine='svg', line_width=20, line_spacing=10, outline_width=1, render_dir_markers=False, no_render_stations=False, tight_stations=False, no_render_node_connections=False, labels=False, z=[14,15,16,17]):
+        transitmap_cfg=f'transitmap --line-width={line_width} --line-spacing={line_spacing} --outline-width={outline_width}'
+        if render_dir_markers==True:
+            transitmap_cfg+=f' --render-dir-markers'
+        if no_render_stations==True:
+            transitmap_cfg+=f' --no-render-stations'
+        if tight_stations==True:
+            transitmap_cfg+=f' --tight-stations'
+        if no_render_node_connections==True:
+            transitmap_cfg+=f' --no-render-node-connections'
+        if labels==True:
+            transitmap_cfg+=f' --labels'
+        if render_engine=='svg':
+            transitmap_cfg+=f' --render-engine=svg > {self.selection_folder}/stop_{self.stop_id}.svg'
+        elif render_engine=='mvt':
+            transitmap_cfg+=f' --render-engine=mvt -z {",".join(map(str, z))} --mvt-path={self.selection_folder}'
+        transitmap_cfg+=f' < {self.selection_folder}/loom.json'
+        try:
+            transitmap_subp=subprocess.check_output(transitmap_cfg, shell=True)
+        except subprocess.CalledProcessError as exc:                                                                                                   
+            print("error code", exc.returncode, exc.output)
+    def decode_mvt(self, zl=14):
+        def pixel2deg(xtile, ytile, zoom, xpixel, ypixel, extent = 4096):
+            # from https://gis.stackexchange.com/a/460173/44746
+            n = 2.0 ** zoom
+            xtile = xtile + (xpixel / extent)
+            ytile = ytile + ((extent - ypixel) / extent)
+            lon_deg = (xtile / n) * 360.0 - 180.0
+            lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
+            lat_deg = math.degrees(lat_rad)
+            return (lon_deg, lat_deg)
+
+        inner_connections=[]
+        lines=[]
+        stations=[]
+        zoomfolder=f'/{self.selection_folder}/{zl}'
+        for subdir, dirs, files in os.walk(zoomfolder):
+            for file in files:
+                #print(os.path.join(subdir, file))
+                tile=os.path.join(subdir, file)
+                with open(tile, 'rb') as f:
+                    data=f.read()
+                tile_zoom=int(tile.split('/')[-3])
+                tile_x=int(tile.split('/')[-2])
+                tile_y=int(tile.split('/')[-1][:-4])
+                decoded_data = mapbox_vector_tile.decode(data, transformer = lambda x, y: pixel2deg(tile_x, tile_y, tile_zoom, x, y))
+                inner_connections.append(decoded_data['inner-connections'])
+                lines.append(decoded_data['lines'])
+                stations.append(decoded_data['stations'])
+        return lines, stations, inner_connections
+            
 
 class Feed:
     def __init__(self, folder: str) -> None:
         # Initialize feed by importing csv to df
-        self.agency=pd.read_csv(folder+r'\agency.txt', sep=',')
-        self.calendar=pd.read_csv(folder+r'\calendar.txt', sep=',')
-        self.calendar_dates=pd.read_csv(folder+r'\calendar_dates.txt', sep=',')
-        self.routes=pd.read_csv(folder+r'\routes.txt', sep=',')
-        self.trips=pd.read_csv(folder+r'\trips.txt', sep=',')
-        self.stops=pd.read_csv(folder+r'\stops.txt', sep=',')
-        self.stop_times=pd.read_csv(folder+r'\stop_times.txt', sep=',')
-        self.shapes=pd.read_csv(folder+r'\shapes.txt', sep=',')
+        self.agency=pd.read_csv(folder+'/agency.txt', sep=',')
+        self.calendar=pd.read_csv(folder+'/calendar.txt', sep=',')
+        self.calendar_dates=pd.read_csv(folder+'/calendar_dates.txt', sep=',')
+        self.routes=pd.read_csv(folder+'/routes.txt', sep=',')
+        self.trips=pd.read_csv(folder+'/trips.txt', sep=',')
+        stops=pd.read_csv(folder+'/stops.txt', sep=',')
+        self.stops=gpd.GeoDataFrame(stops, geometry=gpd.points_from_xy(stops.stop_lon, stops.stop_lat), crs="EPSG:4326")
+        #self.stops=pd.read_csv(folder+'/stops.txt', sep=',')
+        self.stop_times=pd.read_csv(folder+'/stop_times.txt', sep=',')
+        shapes=pd.read_csv(folder+'/shapes.txt', sep=',')
+        self.shapes=gpd.GeoDataFrame(shapes, geometry=gpd.points_from_xy(shapes.shape_pt_lon, shapes.shape_pt_lat))
+    def RTSelection(self, trip_ids, t, r):
+        # Выборка маршрутов, соответствующих выбранным поездкам
 
+        # На вход: 
+        # trip_ids - список идентификаторов поездок
+        # t - dataframe всех поездок маршрутов
+        # r - dataframe всех маршрутов 
+
+        # Все поездки, соответствующие выборке
+        t_selection=t[t.trip_id.isin(trip_ids)]
+        # Список уникальных идентификаторов маршрутов
+        route_ids=list(set(t_selection.route_id))
+        # Выборка маршрутов по идентификаторам
+        r_selection=r[r.route_id.isin(route_ids)]
+        return t_selection, r_selection
+    def SSelection(self, st_selection):
+        stop_ids=list(set(st_selection.stop_id))
+        s_selection=self.stops[self.stops.stop_id.isin(stop_ids)]
+        return s_selection
+    def STSelection(self, stop_id, st):
+        # Выборка поездок маршрутов, проходящих через остановку
+
+        # На вход: 
+        # stop_id - идентификатор остановки
+        # st - dataframe всех поездок маршрутов
+
+        # Все поездки, проходящие через остановку
+        st_selection=st[st.stop_id==stop_id]
+
+        # Пустой dataframe, в который будет внесены выборки поездок маршрутов
+        st_empty=pd.DataFrame(columns=st.columns)
+        for _, trip in st_selection.iterrows():
+            # Выборка поездок от момента прибытия на остановку stop_id до конца поездки
+            #print(st)
+            st_serving=st.loc[(trip.trip_id==st.trip_id)]
+            #print(st_serving.iloc[0], '\n', st_serving.iloc[len(st_serving)-1])
+            #Если маршрут круговой (ID остановок в начале и конце совпадают), то выбрать всю последовательность
+            if st_serving.iloc[len(st_serving)-1]['stop_id']==st_serving.iloc[0]['stop_id']:
+                #print(st_serving)
+                #st_serving=st_serving.drop_duplicates(subset=['trip_id', 'stop_sequence', 'stop_id'], keep='first')
+                print(st_serving.value_counts())
+            else:
+                st_serving=st.loc[(trip.trip_id==st.trip_id) & (trip.stop_sequence<=st.stop_sequence)]
+                # Обновление порядка остановок
+                st_serving['stop_sequence']=st_serving['stop_sequence']-trip.stop_sequence+1
+            # Конкатенация пустого dataframe с выборкой
+            st_empty=pd.concat([st_empty, st_serving])
+            st_empty.drop_duplicates(subset=['trip_id', 'stop_sequence', 'stop_id'], keep='first', inplace=True)
+        return st_empty
+    def ShapeSelection(self, stop, shape_ids, shp):
+        # Выборка геометрии маршрутов
+        # 
+        # На вход: 
+        # stop - остановка с геометрией типа точка, 
+        # shape_ids - выборка идентификаторов геометрии маршрутов,
+        # shp - выборка геометрии маршрутов
+        dist=math.inf
+        seq=None
+        stop_geom=stop.iloc[0].geometry
+        # Пустой dataframe, в который будет внесены выборки геометри маршрутов
+        shp_empty=gpd.GeoDataFrame(columns=shp.columns)
+        for shape_id in shape_ids:
+            # Выборка геометрии маршрута по shape_id
+            shp_selection=shp[shp.shape_id==shape_id]
+            #Если маршрут круговой (геометрия остановок в начале и конце совпадают), то выбрать всю последовательность
+            if shp_selection.iloc[0]['shape_pt_lat']!=shp_selection.iloc[len(shp_selection)-1]['shape_pt_lat']:
+                # Нахождение точки маршрута, расстояние от которой до остановки наименьшее
+                for  _, shape_point in shp_selection.iterrows():
+                    meas=stop_geom.distance(shape_point.geometry)
+                    if meas<=dist:
+                        dist=meas
+                        seq=shape_point.shape_pt_sequence
+                # Найдена точка маршрута, расстояние от которой до остановки наименьшее.
+                # Выборка из dataframe геометрии маршрута: seq - порядок прохождения точки маршрута.
+                # Необходимо в пустой dataframe добавить все точки маршрута, которые по порядку будут не ниже, чем seq
+                shp_serving=shp_selection[shp_selection.shape_pt_sequence>=seq]
+            # Обновление порядка прохождения точек маршрута
+                shp_serving['shape_pt_sequence']=shp_serving['shape_pt_sequence']-seq+1
+            else:
+                shp_serving=shp_selection
+            # Конкатенация пустого dataframe с выборкой
+            shp_empty=pd.concat([shp_empty, shp_serving])
+        return shp_empty
+    
+    def GetGeomOfStop(self, stop_id):
+        stop_geom=self.stops[self.stops.stop_id==stop_id]
+        stop_times_selection=self.STSelection(stop_id, self.stop_times)
+        stops_selection=self.SSelection(stop_times_selection)
+        stop_times_selection_merge=stop_times_selection.merge(self.trips, on='trip_id', how='left')
+        trip_ids=list(set(stop_times_selection_merge.trip_id))
+        trips_selection, routes_selection=self.RTSelection(trip_ids, self.trips, self.routes)
+        shape_ids=list(set(stop_times_selection_merge.shape_id))
+        shape_selection=self.ShapeSelection(stop_geom, shape_ids, self.shapes)
+        return [stops_selection, stop_times_selection, trips_selection, shape_selection, routes_selection]
+    
+    def SaveSelectionOfStop(self, stop_id, folder):
+        stop_geom=self.stops[self.stops.stop_id==stop_id]
+        stop_times_selection=self.STSelection(stop_id, self.stop_times)
+        stops_selection=self.SSelection(stop_times_selection)
+        stop_times_selection_merge=stop_times_selection.merge(self.trips, on='trip_id', how='left')
+        trip_ids=list(set(stop_times_selection_merge.trip_id))
+        trips_selection, routes_selection=self.RTSelection(trip_ids, self.trips, self.routes)
+        shape_ids=list(set(stop_times_selection_merge.shape_id))
+        shape_selection=self.ShapeSelection(stop_geom, shape_ids, self.shapes)
+
+        subprocess.run(f'rm -rf {folder}/stop_{stop_id}/*', stderr=subprocess.STDOUT, shell=True)
+        subprocess.run(f'mkdir -p {folder}/stop_{stop_id}', stderr=subprocess.STDOUT, shell=True)
+        subprocess.run(f'mkdir -p {folder}/stop_{stop_id}/gtfs_edited', stderr=subprocess.STDOUT, shell=True)
+
+        stops_selection.drop('geometry', axis=1).to_csv(f'{folder}/stop_{stop_id}/gtfs_edited/stops.txt', index=False, sep=',', header=True)
+        stop_times_selection.to_csv(f'{folder}/stop_{stop_id}/gtfs_edited/stop_times.txt', index=False, sep=',', header=True)
+        trips_selection.to_csv(f'{folder}/stop_{stop_id}/gtfs_edited/trips.txt', index=False, sep=',', header=True)
+        routes_selection.to_csv(f'{folder}/stop_{stop_id}/gtfs_edited/routes.txt', index=False, sep=',', header=True)
+        self.agency.to_csv(f'{folder}/stop_{stop_id}/gtfs_edited/agency.txt', index=False, sep=',', header=True)
+        self.calendar.to_csv(f'{folder}/stop_{stop_id}/gtfs_edited/calendar.txt', index=False, sep=',', header=True)
+        self.calendar_dates.to_csv(f'{folder}/stop_{stop_id}/gtfs_edited/calendar_dates.txt', index=False, sep=',', header=True)
+        shape_selection.drop('geometry', axis=1).to_csv(f'{folder}/stop_{stop_id}/gtfs_edited/shapes.txt', index=False, sep=',', header=True)
+        return f'{folder}/stop_{stop_id}'
+    
     def StopInfo(self, stop_id):
         return self.stops[self.stops.stop_id==stop_id]
     
@@ -44,10 +277,10 @@ class Feed:
             max=route_arrivals.arrival_time.max()
             min=route_arrivals.arrival_time.min()
             # Нет учета возможных укоротов/вариантов
-            main_headsign=route_arrivals.trip_headsign.mode().item()
+            main_headsign=route_arrivals.trip_headsign.mode()
             route_info[route_id]={'start': min, 'end': max, 'headsign': main_headsign,
                                   'route_color': route['route_color'].item(), 
-                                  'route_text_color': route['route_text_color'].item(), 'route_short_name': route['route_short_name'].item()}
+                                  'route_text_color': route['route_text_color'], 'route_short_name': route['route_short_name'].item()}
         return {k: v for k, v in sorted(route_info.items(), key=lambda item: item[1]['route_short_name'])}
 
     def GetActualServiceID(self, stop_arrivals, stop_routes, service_keys):
